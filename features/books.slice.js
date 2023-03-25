752c11d2e22f78addb8d47508b2b02ec4eaf5697
i2xv <iscoramadane@gmail.com>
2023-03-25 06:10:38 +0100

    update

--- features/books.slice.js
+++ features/books.slice.js
@@ -10,30 +10,8 @@ export const bookSlice = createSlice({
     setBookId: (state, { payload }) => {
       state.bookId = payload;
     },
-    setBook: (state, { payload }) => {
-      state.pictures = payload;
-    },
-    addBook: (state, { payload }) => {
-      state.book.push(payload);
-    },
-    editBook: (state, { payload }) => {
-      state.book = state.book.map((pic) => {
-        if (pic.id === payload[1]) {
-          return {
-            ...pic,
-            artist: payload[0],
-          };
-        } else {
-          return pic;
-        }
-      });
-    },
-    deleteBook: (state, { payload }) => {
-      state.book = state.book.filter((pic) => pic.id !== payload);
-    },
   },
 });
 
-export const { setBookId, addBook, editBook, deleteBook, setBook } =
-  bookSlice.actions;
+export const { setBookId } = bookSlice.actions;
 export default bookSlice.reducer;
